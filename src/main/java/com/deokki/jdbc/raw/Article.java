9fb68b4c1b532cb887ec956a79476de88fc02a90
박덕기 <deokki3@gmail.com>
2019-03-22 17:40:12 +0900

    add src

---
+++ src/main/java/com/deokki/jdbc/raw/Article.java
@@ -0,0 +1,57 @@
+package com.deokki.jdbc.raw;
+
+public class Article {
+	String articleId;
+	String title;
+	String content;
+	String UserId;
+	String name;
+	String cdate;
+	
+	public String getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUserId() {
+		return UserId;
+	}
+	public void setUserId(String userId) {
+		UserId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+	@Override
+	public String toString() {
+		return "Article [articleId=" + articleId + ", title=" + title + ", content=" + content + ", UserId=" + UserId
+				+ ", name=" + name + ", cdate=" + cdate + "]";
+	}
+
+	
+	
+	
+
+}
